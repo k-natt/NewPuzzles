8a9823ced8a267db924f20d549175747dc9e5ae6
Kevin Nattinger <git@nattinger.net>
2021-12-27 15:12:36 -0800

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Kevin on 12/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/NewPuzzlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewPuzzlesApp.swift
+//  Shared
+//
+//  Created by Kevin on 12/27/21.
+//
+
+import SwiftUI
+
+@main
+struct NewPuzzlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
